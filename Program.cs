cd7e6e3e0b005329d9101efe6748ca5d49a127ac
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-06-13 15:18:19 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+while (true)
+{
+    Write("Value: ");
+    string? value = ReadLine();
+    WriteLine($"Hash: {(value is null ? "null" : value.GetHashCode())}");
+    WriteLine();
+}
